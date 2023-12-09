50e93ee7d049a00b52cf1e2b54d6a9f7f85b79cf
Edson Lima Costa <edsonlimajll@gmail.com>
2023-12-09 02:20:15 -0300

    add ex handler validation errors

--- src/main/java/br/com/edsoncosta/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/edsoncosta/webfluxcourse/model/request/UserRequest.java
@@ -1,8 +1,20 @@
 package br.com.edsoncosta.webfluxcourse.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
+        @NotBlank(message = "must not be null or empty")
         String name,
+
+        @Email
         String email,
+
+        @NotBlank(message = "must not be null or empty")
+        @Size(min = 3, max = 20, message = "must be between 3 and 20 characters")
         String password
 ) {
 }
