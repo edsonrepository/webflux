23e4b87140b28b66dcaae93e2018ebb3305460d8
Edson Lima Costa <edsonlimajll@gmail.com>
2023-12-10 01:57:46 -0300

    add impl to contract findById user

--- src/main/java/br/com/edsoncosta/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/edsoncosta/webfluxcourse/model/request/UserRequest.java
@@ -1,18 +1,22 @@
 package br.com.edsoncosta.webfluxcourse.model.request;
 
+import br.com.edsoncosta.webfluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
 
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         @NotBlank(message = "must not be null or empty")
         String name,
 
+        @TrimString
         @Email
         String email,
 
+        @TrimString
         @NotBlank(message = "must not be null or empty")
         @Size(min = 3, max = 20, message = "must be between 3 and 20 characters")
         String password
--- src/main/java/br/com/edsoncosta/webfluxcourse/repository/UserRepository.java
+++ src/main/java/br/com/edsoncosta/webfluxcourse/repository/UserRepository.java
@@ -14,4 +14,8 @@ public class UserRepository {
     public Mono<User> save(final User user) {
         return mongoTemplate.save(user);
     }
+
+    public Mono<User> findById(String id) {
+        return mongoTemplate.findById(id, User.class);
+    }
 }
